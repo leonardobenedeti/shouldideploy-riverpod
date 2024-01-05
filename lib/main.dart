58718d248e6b556c1622b614a99217eedcebfd4f
Leonardo Benedeti <leonardobenedeti@gmail.com>
2024-01-04 23:41:04 -0300

    Primeiros providers: counter e should did deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,25 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -36,90 +28,164 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider);
+    final AsyncValue<ShouldDidDeployToday> response =
+        ref.watch(shouldDidDeployTodayProvider);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    final textStyle = Theme.of(context)
+        .textTheme
+        .headlineMedium
+        ?.copyWith(color: Colors.white);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey[200],
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () =>
+                            ref.read(counterProvider.notifier).decrement(),
+                        icon: const Icon(Icons.remove),
+                      ),
+                      Text(
+                        '$counter',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      IconButton(
+                        onPressed: () =>
+                            ref.read(counterProvider.notifier).increment(),
+                        icon: const Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () => ref.read(counterProvider.notifier).build(),
+                    child: const Text('Reiniciar contagem'),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              height: 200,
+              child: Center(
+                child: switch (response) {
+                  AsyncData(:final value) => Container(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color:
+                            value.shouldideploy ? Colors.grey : Colors.red[300],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            'Should did deploy today?',
+                            textAlign: TextAlign.center,
+                            style: textStyle,
+                          ),
+                          Text(
+                            value.message,
+                            textAlign: TextAlign.center,
+                            style: textStyle,
+                          ),
+                          TextButton(
+                            onPressed: () => ref
+                                .refresh(shouldDidDeployTodayProvider.future),
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.white,
+                            ),
+                            child: const Text('Try Again'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  AsyncLoading() =>
+                    const Center(child: CircularProgressIndicator()),
+                  AsyncError() => Text(
+                      'Oops, something unexpected happened',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  _ => const CircularProgressIndicator(),
+                },
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+@riverpod
+class Counter extends _$Counter {
+  @override
+  int build() => state = 0;
+
+  void increment() => state++;
+
+  void decrement() => state--;
+}
+
+@riverpod
+Future<ShouldDidDeployToday> shouldDidDeployToday(
+    ShouldDidDeployTodayRef ref) async {
+  final response = await http.get(
+      Uri.https('shouldideploy.today', '/api', {'tz': 'America/Sao_Paulo'}));
+  log(response.body);
+  return ShouldDidDeployToday.fromJson(response.body);
+}
+
+class ShouldDidDeployToday {
+  String timezone;
+  DateTime date;
+  bool shouldideploy;
+  String message;
+
+  ShouldDidDeployToday({
+    required this.timezone,
+    required this.date,
+    required this.shouldideploy,
+    required this.message,
+  });
+
+  factory ShouldDidDeployToday.fromMap(Map<String, dynamic> map) {
+    return ShouldDidDeployToday(
+      timezone: map['timezone'] as String,
+      date: DateTime.parse(map['date']),
+      shouldideploy: map['shouldideploy'] as bool,
+      message: map['message'] as String,
+    );
+  }
+
+  factory ShouldDidDeployToday.fromJson(String source) =>
+      ShouldDidDeployToday.fromMap(json.decode(source) as Map<String, dynamic>);
 }
